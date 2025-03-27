a98f2c5d6787e688d78139f7a2575933843dd951
emba2ra3star <tachi.i.061@gmail.com>
2025-03-28 02:57:23 +0800

    fix img path for vecel

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -2,7 +2,7 @@ function BookDetail({ data }) {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-24 gap-8 justify-center">
             <div className="lg:col-start-3 lg:col-span-6">
-                <img className="w-full" src="/src/images//book2.png" />
+                <img className="w-full" src={data.cover} />
             </div>
             <div className="lg:col-span-14 text-left">
                 <h1 className="text-white text-2xl font-bold mb-2">{data.title}</h1>
